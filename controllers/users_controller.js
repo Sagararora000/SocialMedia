bdcedb243c4add74a02d1ca042ccd067b6868330
Sagar <sagar@beanbyte.in>
2023-11-12 23:53:23 +0530

    Added sign up and sign in page to render for manual authentication later

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,6 +3,48 @@ module.exports.users = function(req,res){
         title:"Basic users page"
     });
 }
-module.exports.hello = function(req,res){
-    return res.end("<h1>Hi this is res.end</h1>");
+module.exports.signUp = function(req,res){
+    return res.render("user_sign_up",{
+        title:"Sign Up"
+    })
+}
+
+module.exports.signIn = function(req,res){
+    return res.render("user_sign_in",{
+        title:"Sign In"
+    })
+}
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect("back");
+    }
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){
+            console.log("error in finding user in sign-up");
+            return;
+        }
+        if(!user){
+            user.create(req.body, function(err,user){
+                if(err){
+                    console.log("error in creating user while signing up");
+                    return;
+                }
+                return res.redirect("/users/sign-in");
+            });
+        }else{
+            return res.redirect("back");
+        }
+    });
+}
+//sign in and create a session for the user
+//Part of Manual Authentication
+module.exports.createSession = function(req,res){
+    //Steps to authenticate 
+    //find the user
+
+    //handle password which don't match
+
+    //handle session creation
+
+    //handle user not found
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/',usersController.users);
-router.get('/profile',usersController.hello);
-
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+router.get('/create',usersController.create);
 return module.exports = router;
