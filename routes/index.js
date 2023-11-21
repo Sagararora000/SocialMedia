4efc27f67311f698bde4de3aaa2e433c954cd8f4
Sagar <sagar@beanbyte.in>
2023-11-22 02:01:14 +0530

    Corrected previous commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
-router.use('/',homeController.home);
+router.get('/',homeController.home);
 // This is use middle ware for further routes
 router.use('/postFeed',require('./posts'));
 router.use('/users',require('./users'));
